9b9676318a928aac5cac3bf8d42d4f246abcfa75
KC-00001 <kcarbokevin9@gmail.com>
2025-05-14 23:33:55 -0500

    Agregado de componentes de home/signin y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LogInComponent},
+    {path: 'signin', component: SignInComponent},
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
