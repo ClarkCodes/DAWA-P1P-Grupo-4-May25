60d176dded8a2d0a9532cbcb8b5b767bad6007bb
KC-00001 <kcarbokevin9@gmail.com>
2025-05-20 09:38:46 -0500

    finalizado el componente de login/signin y avance de la tabla

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -3,16 +3,18 @@ import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angula
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { CuentasService } from '../../services/SigninLogin/cuentas.service';
-import { Cuentas } from '../../models/cuentas';
 import { ServFacultadDatosService } from '../../services/SigninLogin/serv-facultad-datos.service';
 import { FacultadDatos } from '../../models/facultadDatos';
 import { MatFormField } from '@angular/material/input';
 import { MatLabel } from '@angular/material/form-field';
-import { MatOption } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSelect } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
+import { Cuentas } from '../../models/cuentas';
+import { FormGroup } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -23,12 +25,13 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule, 
     MatFormFieldModule, 
     FormsModule, 
-    ReactiveFormsModule, 
-    MatOption,
+    ReactiveFormsModule,
     MatIconModule,
-    MatSelect,
     MatRadioModule,
     MatButtonModule,
+    NgIf,
+    MatSelectModule
+
     ],
   standalone: true,
   templateUrl: './sign-in.component.html',
@@ -37,16 +40,49 @@ import { MatButtonModule } from '@angular/material/button';
 
 export class SignInComponent implements OnInit {
 
+nombreControl = new FormControl('', Validators.required);
+passwordControl = new FormControl('', Validators.required);
+rolControl = new FormControl('', Validators.required);
+
+  userForm: FormGroup;
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const userData = this.userForm.value;
+      console.log('Datos del formulario:', userData);
+
+      // Llamar servicio para guardar (asumiendo que servicioCuentas tiene método saveUser)
+      this.servicioCuentas.addCuentas(userData).subscribe({
+        next: (response) => {
+          console.log('Usuario guardado', response);
+          this.clearForm();
+        },
+        error: (error) => {
+          console.error('Error al guardar usuario', error);
+        }
+      });
+    } else {
+      console.warn('Formulario inválido');
+    }
+  }
+
+  clearForm() {
+    this.userForm.reset();
+  }
+
+
 facultadControl = new FormControl< | null>(null, Validators.required);
   facultades: FacultadDatos[] = [];
 
-  categoriaControl = new FormControl<FacultadDatos | null>(null, Validators.required);
-  categorias: FacultadDatos[] = [];
-
-  constructor(private servicioFacultadDatos: ServFacultadDatosService ) {
-  }
-
-  
+  constructor(private servicioFacultadDatos: ServFacultadDatosService, private servicioCuentas: CuentasService) {
+     this.userForm = new FormGroup({
+      nombre: this.nombreControl,
+      email: this.emailFormControl,
+      password: this.passwordControl,
+      facultad: this.facultadControl,
+      rol: this.rolControl
+    });
+ }
 
   ngOnInit(): void {
      this.servicioFacultadDatos.getFacultadDatos().subscribe({
