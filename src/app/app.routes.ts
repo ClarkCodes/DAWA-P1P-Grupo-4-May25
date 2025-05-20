6be7f0458860eaf9eed273ff75fac9401d85ffdc
CinthiaTenesaca <cinthia2003maria@gmail.com>
2025-05-20 12:28:25 -0500

    Componente Comentario agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 import { CrudFacultadesComponent } from './components/crud-facultades/crud-facultades.component';
+import { ComentariosComponent } from './components/comentarios/comentarios.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LogInComponent },
   { path: 'signin', component: SignInComponent },
   { path: 'crud-facultades', component: CrudFacultadesComponent },
+  { path: 'comentarios', component: ComentariosComponent },
   { path: '404', component: PaginaNoEncontradaComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: "**", redirectTo:"404" },
