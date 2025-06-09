53a496be961c9a9f6d2104f59e4bd5cdc6349cc4
Clark <clark.codes.sk@gmail.com>
2025-06-08 19:53:02 -0500

    Agrega providers al archivo de configuracion global
    Estos nuevos providers permiten tener caracteristicas de personalizacion en diferentes componentes
    
    Estas caracteristicas estan relacionadas con localizacion de la aplicacion para visualizacion de fechas y horas, visualizacion por defecto de las cajas de formulario ajustadas a outline, visualizacion de iconos personalizados en pasos de un MatStepper
    
    Los componentes que se benefician son: MatFormField, MatDatePicker, MatTimePicker, MatStepper y alguno otro que tambien utilice las caracteristicas incorporadas
    
    Esto permite una correcta visualizacion de datos en nuestra region y brinda un aspecto mas refinado, trabajado, profesional y hecho para los usuarios objetivos, mejorando el sitio considerablemente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'es' },
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    { provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false, showError: true } },
+    { provide: MAT_DATE_LOCALE, useValue: 'es-EC' }
   ]
 };
