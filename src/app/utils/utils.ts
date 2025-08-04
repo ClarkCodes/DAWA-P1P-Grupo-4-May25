a1896e7c0a5833caebe581ba2f88228f7e897862
Clark <clark.codes.sk@gmail.com>
2025-08-04 02:19:05 -0500

    Implementa el Pipe TimeAgo
    Feature: Principalmente se implementa el pipe TimeAgo para descripcion de lapsos de tiempo relativos en funcion de fechas dadas
    
    Esto sera especialmente util en los comentarios de los eventos, para describir hace cuanto se crearon
    
    Este pipe usa la popular dependencia date-fns para su proposito
    
    Este pipe se ubica dentro de la carpeta utils con el nombre time-ago.pipe.ts
    
    Chore: Se mueve la carpeta utils de la carpeta 'src/app/components/shared' a 'src/app', dado que el contenido de la carpeta utils no son componentes en si, son clases y herramientas de utilidad, por lo que tiene mas sentido que esten a nivel de aplicacion, en la carpeta de destino actual

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,73 @@
+import { Observable } from "rxjs";
+
+export enum RolEnum {
+  ADMINISTRADOR = 1,
+  ESTUDIANTE = 2,
+  FACULTAD = 3,
+  CLUB = 4
+}
+
+/**
+ * Handles image loading errors by setting a default image source, so it sets the source of the image to a default "no image found" placeholder.
+ * @param imgElement The image element that will have its source changed to a default image if an error occurs,
+ * this is typically used in the `onerror` event of an image to handle cases where the image fails to load
+ */
+export function onImageError( imgElement: HTMLImageElement ) {
+  imgElement.src = '/images/no-image-found-design.webp';
+}
+
+export function getUserName( name: string ): string {
+  const fullName = name.split( ' ' ) ?? 'Usuario';
+  const firstName = Array.isArray( fullName ) ? fullName[0] : fullName;
+  const secondName = fullName[1] ?? '';
+  return secondName ? ( `${firstName} ${secondName}` ) : firstName;
+}
+
+export function getRolEnumKeyNameByStrId( strId: string ): string | undefined {
+  return RolEnum[Number(strId)];
+}
+
+export function getRolEnumByStrId( strId: string ): RolEnum | undefined {
+  switch( strId ) {
+    case '1':
+      return RolEnum.ADMINISTRADOR;
+    case '2':
+      return RolEnum.ESTUDIANTE;
+    case '3':
+      return RolEnum.FACULTAD;
+    case '4':
+      return RolEnum.CLUB;
+  }
+
+  return;
+}
+
+/** Interface to ensure that the object has an `id` property of type number, this is useful for functions that need to generate a new ID based on existing IDs */
+interface Identifiable {
+  id: number;
+}
+
+/**
+ * Generates a new ID based on the maximum ID found in the provided source.
+ * @param idSource Source of IDs, typically an Observable that emits an array of items with an `id` property,
+ * in this case it would be commonly the result of a service function that returns an Observable of an array of a certain type of items
+ * @returns The top id + 1, or undefined if the source is empty or not provided
+ */
+export function generateNewId<T extends Identifiable>( idSource: Observable<T[]> ): number | undefined {
+  idSource.subscribe( ( items: T[] ) => {
+    if( items ){
+      let topId = 0;
+
+      items.forEach( item => {
+        if( item.id > topId )
+          topId = item.id;
+      });
+
+      return topId + 1;
+    }
+
+    return;
+  });
+
+  return;
+}
