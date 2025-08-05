9859e68695a90fcd15b088f15f8a129d0d08c2fb
Clark <clark.codes.sk@gmail.com>
2025-08-05 06:06:39 -0500

    Actualizacion del Header y la NavBar principal
    - Update: Se actualiza el componente principal de la aplicacion, el componente app, el cual tiene basicamente el Header, el Footer y en medio el router-outlet, que es el contenido de la pagina, que son los componentes de pagina correspondientes a las rutas establecidas en el archivo app.routes.ts
    
    - Feature: Se implementan links de navegacion en la NavBar del Header que estaran centrados, cada link con un icono, dos de estos links por el momento son fijos, estos son Inicio y Eventos, y luego estan links renderizados condicionalmente en funcion del rol del usuario, si son gestores de facultad o de club se les muestra un link apropiado para Gestionar su tipo de evento correspondiente
    
    - Feature: Renderizado condicional de Opciones de Sesion, se implementa esta feature requerida, dependiendo del estado de sesion se muestran unos elementos u otros, si no se ha iniciado sesion se muestra los botones de Iniciar Sesion y Registrarse, si ya se ha iniciado sesion se muestra el Avatar con la imagen de perfil del usuario y el nombre de este, y solo si el rol de administrador se muestra el boton link del Panel Admin para ir a la ruta del panel de administrador
    
    Tambien se implementan estrategias responsive, lo cual hace que si el ancho de la ventana del navegador o de la pantalla se vuelve pequenio, usando media queries para detectar el ancho, los links del navbar y las opciones de sesion se mostraran en un menu el cual se puede desplegar con su boton ubicado a la derecha, de esta manera las opciones se muestran en un practico menu pequenio de manera vertical y no se montan
    
    Sin embargo si aparece un indicador en la mitad de la NavBar que indica en que pagina se encuentra el usuario, con el nombre de la pagina, el cual tambien es el nombre del link y su icono correspondiente
    
    Se implementan los estilos necesarios para que visualmente se vean todos los elementos bien, balanceados, alineados como corresponden, y tambien se implementa la logica en su archivo ts para las funcionalidades correspondientes
    
    Con esto se ha implementado renderizado condicional del Header, en los links de navegacion y las opciones de sesion, asi como dotarlo de capacidad Responsive, ahora el Header es responsive
    
    - Update: Se actualiza el archivo de estilos globales styles.css
    
    Ahora el Header tiene un position: fixed, lo cual hace que se quede fijo cuando se hace scroll, bastante adecuado, eso hace que el contenido del main suba, dado que ahora ya no se reserva el espacio del Header dado que estaria como flotando, para ello se adapta en el selector app-root la plantilla del grid-template-rows para que el Footer siga estando al fondo
    
    Se ajustan los estilos de algunos titulos, elementos h1 y h2 para que se adapten
    
    Se mueve el la clase spacer al archivo styles.css, antes estaba en el archivo de estilos de app.css, esto para que se pueda utilizar en otros componentes
    
    Se trasladan estilos referentes a mat-card-actions, DetallesContainer y BodyClass, que basicamente son estilos de card usados por el card de eventos de Facultad y que se planean usar tambien en el card de evento de Club para renderizado condicional de elementos en estas, pero que tambien se usan en el componente de Detalles de Evento, por lo que conviene tener estos estilos accesibles para diferentes componentes, por ello se han trasladado a este archivo global de estilos
    
    - Update: Se actualiza el archivo de rutas app.routes.ts, removiendo las rutas que ya no estan vigentes o de componentes de pagina que se han eliminado y se agregan o se actualizan las rutas de los componentes actuales y los nuevos que se han implementado como la ruta de eventos, perfil, signup, crud-admin-panel, etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,114 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, HostListener, inject, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CuentasService } from './services/SignupLogin/cuentas.service';
+import { Cuenta } from './models/cuenta';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { getUserName, RolEnum } from './utils/utils';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatTooltipModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private router = inject( Router );
+  public cuentasService = inject( CuentasService );
+  public rolEnum = RolEnum;
+  public usuarioLogueadoRolId: number = 0;
+  usuarioLogueado: Cuenta | null = null;
+
+  @ViewChildren( MatMenuTrigger ) menuTriggers!: QueryList<MatMenuTrigger>;
+
+  ngOnInit() {
+    this.cuentasService.usuarioLogueado$.subscribe( cuenta => {
+      this.usuarioLogueado = cuenta;
+      this.usuarioLogueadoRolId = Number( cuenta?.idRol );
+    });
+  }
+
+  @HostListener( 'window:resize' ) //, ['$event']
+  onWindowResize() {
+    this.menuTriggers.forEach( trigger => { // Iterate over all the triggers and close any that are open
+      if ( trigger.menuOpen )
+        trigger.closeMenu();
+    });
+  }
+
+  getUserName(): string {
+    return getUserName( this.usuarioLogueado?.nombre as string );
+  }
+
+  getPageTitleIcon(): string {
+    switch( this.getCurrentUrl() ){
+      case '/home':
+      case '/':
+        return 'home';
+      case '/eventos':
+        return 'event';
+      case '/crud-eventos-facultades':
+      case '/crud-eventos-clubes':
+        return 'event_upcoming';
+      case '/crud-signin-login':
+        return 'admin_panel_settings';
+      case '/perfil':
+        return 'person';
+      case '/login':
+        return 'login';
+      case '/signin':
+        return 'signature';
+      default:
+        return 'help';
+    }
+  }
+
+  getPageTitleName() {
+    switch( this.getCurrentUrl() ){
+      case '/home':
+      case '/':
+        return 'Inicio';
+      case '/eventos':
+        return 'Eventos';
+      case '/crud-eventos-facultades':
+        return 'Gestion de Eventos(Facultad)';
+      case '/crud-eventos-clubes':
+        return 'Gestion de Eventos(Club)';
+      case '/crud-signin-login':
+        return 'Visualización';
+      case '/perfil':
+        return 'Perfil';
+      case '/login':
+        return 'Iniciar Sesión';
+      case '/signin':
+        return 'Registrarse';
+      default:
+        return 'help';
+    }
+  }
+
+  private getCurrentUrl() {
+    return this.router.url;
+  }
+
+  logout() {
+    this.cuentasService.logout();
+    this.usuarioLogueadoRolId = 0;
+    this.router.navigate(['/home']);
+  }
 }
